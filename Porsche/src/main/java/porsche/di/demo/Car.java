3010e3996f0fa7f09dc3e8cb1c1bbbbb67cf8bc7
Lakshana0729 <lakshanaammu2907@gmail.com>
2025-12-19 13:13:48 +0530

    Car Example Spring Framework

---
+++ Porsche/src/main/java/porsche/di/demo/Car.java
@@ -0,0 +1,14 @@
+package porsche.di.demo;
+
+public class Car {
+	
+	private Wheel wheelObj;
+	
+	public Wheel getWheelObj() {
+		return wheelObj;
+	}
+	
+	public void setWheelObj(Wheel wheelObj) {
+		this.wheelObj = wheelObj;
+	}
+}
---
+++ Porsche/src/main/java/porsche/di/demo/Wheel.java
@@ -0,0 +1,14 @@
+package porsche.di.demo;
+
+public class Wheel {
+	
+	private int wheelsize;
+	
+	public int getWheelsize() {
+		return wheelsize;
+	}
+	
+	public void setWheelsize(int wheelsize) {
+		this.wheelsize = wheelsize;
+	}
+}
