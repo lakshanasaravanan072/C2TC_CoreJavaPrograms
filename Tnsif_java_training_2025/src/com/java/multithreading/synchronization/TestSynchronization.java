d8b3ea5e9a1a298e6787ae5efc4f4031eda14963
Lakshana0729 <lakshanaammu2907@gmail.com>
2025-12-24 19:54:39 +0530

    Added thread programs

---
+++ Tnsif_java_training_2025/src/com/java/multithreading/synchronization/TestSynchronization.java
@@ -0,0 +1,59 @@
+package com.java.multithreading.synchronization;
+
+//Executor class
+public class TestSynchronization {
+ public static void main(String[] args) {
+
+     Table obj = new Table(); // Shared object
+
+     MyThread1 t1 = new MyThread1(obj);
+     MyThread2 t2 = new MyThread2(obj);
+
+     t1.start();
+     t2.start();
+ }
+}
+
+class Table {
+    // Synchronized method
+	synchronized  void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+// Thread class 1
+class MyThread1 extends Thread {
+    Table t;
+
+    MyThread1(Table obj) {
+        this.t = obj;
+    }
+
+    public void run() {
+        t.printTable(5);
+    }
+}
+
+
+// Thread class 2
+class MyThread2 extends Thread {
+    Table t;
+
+    MyThread2(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(100);
+    }
+}
+
+
+
